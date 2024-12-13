6afd04f2243565514ea2de7c8c0b930f2f2bcd65
johnmapengu <183103110+johnmapengu@users.noreply.github.com>
2024-12-13 14:52:13 +0000

    Delete Assessment1-main directory

--- Assessment1-main/Assessment1-main/README.md
+++
@@ -1,4 +0,0 @@
-#Unit assessments
-1. creating GiftHub repository
-2. Assessment 2
-3. Assessment 3
--- Assessment1-main/Modules_codes/output.py
+++
@@ -1,8 +0,0 @@
-name = {
- "Name1" : "John",
- "Name2" : "Ali",
- "Name3" : "Marko"
-}
-
-# printing the dictionary
-print(name)
